6f061d62a4fb001c8b218449063f657e25232389
Dondi Michael Stroma <dstroma@gmail.com>
2025-07-23 05:03:42 -0400

    First commit (using Minilla for CPAN)

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+PlackX::Framework - It's new $module
+
+# SYNOPSIS
+
+    use PlackX::Framework;
+
+# DESCRIPTION
+
+PlackX::Framework is ...
+
+# LICENSE
+
+Copyright (C) Dondi Michael Stroma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Dondi Michael Stroma <dstroma@gmail.com>
---
+++ lib/PlackX/Framework.pm
@@ -0,0 +1,39 @@
+package PlackX::Framework;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+PlackX::Framework - It's new $module
+
+=head1 SYNOPSIS
+
+    use PlackX::Framework;
+
+=head1 DESCRIPTION
+
+PlackX::Framework is ...
+
+=head1 LICENSE
+
+Copyright (C) Dondi Michael Stroma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Dondi Michael Stroma E<lt>dstroma@gmail.comE<gt>
+
+=cut
+
