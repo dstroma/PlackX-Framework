4dd8231469ab6a73453f0adc0c1c817705bdabd1
Dondi Michael Stroma <dstroma@gmail.com>
2025-07-28 01:13:11 -0400

    Add Router::Engine POD and slight refactor

--- lib/PlackX/Framework/Router/Engine.pm
+++ lib/PlackX/Framework/Router/Engine.pm
@@ -6,6 +6,7 @@ package PlackX::Framework::Router::Engine {
   my %instances;
   sub instance ($class) { $instances{$class} ||= $class->new; }
 
+  # Matching object methods ###########################################
   sub match ($self, $request) {
     # Hack to make Router::Boom match against the HTTP request method
     my $destination = '/['.$request->method.']' . $request->destination;
@@ -44,36 +45,37 @@ package PlackX::Framework::Router::Engine {
     return $matches;
   }
 
-  sub add_route ($router, %params) {
+  # Meta object methods - add route or global filter ##################
+  sub add_route ($self, %params) {
     my $route   = delete $params{routespec};
     my $base    = delete $params{base};
-    my $path    = $route;
 
+    # Validate subroutine params
+    die 'add_route(routespec => STRING|ARRAYREF|HASHREf)'
+      unless ref $route eq 'HASH' or ref $route eq 'ARRAY' or not ref $route;
+
+    # Process hashref like
+    # { get => 'url1', post => 'url2' } or { get => ['url1', 'url2'] }
     if (ref $route eq 'HASH') {
       foreach my $key (keys %$route) {
-        $path = $route->{$key};
-        if (ref $path eq 'ARRAY') {
-          my @paths = @$path;
-          foreach $path (@paths) {
-            $router->add(path_with_base_and_method($path, $base, uc $key), \%params);
-          }
-        } else {
-          $router->add(path_with_base_and_method($path, $base, uc $key), \%params);
-        }
+        my $paths = ref $route->{$key} ? $route->{$key} : [$route->{$key}];
+        $self->add(path_with_base_and_method($_, $base, uc $key), \%params) for @$paths;
       }
-    } elsif (ref $route eq 'ARRAY') {
-      foreach $path (@$route) {
-        $router->add(path_with_base_and_method($path, $base), \%params);
-      }
-    } else {
-      $router->add(path_with_base_and_method($path, $base), \%params);
+      return;
     }
+
+    # String or arrayref without HTTP verb
+    $route = [$route] unless ref $route;
+    $self->add(path_with_base_and_method($_, $base), \%params) for @$route;
+    return;
   }
 
   sub add_global_filter ($self, %params) {
     my $when    = delete $params{'when'};
-    my $pattern = delete $params{'pattern'};
-    my $action  = delete $params{'action'};
+    my $pattern = delete $params{pattern};
+    my $action  = delete $params{action};
+
+    # Validate subroutine params
     die q{Usage: add_global_filter(when => 'before'|'after', ...)}
       unless $when eq 'before' or $when eq 'after';
 
@@ -83,6 +85,7 @@ package PlackX::Framework::Router::Engine {
     push @{$self->{$hkey}}, { pattern => $pattern, action => $action };
   }
 
+  # Helper functions ##################################################
   sub path_with_base ($path, $base) {
     return $path unless $base and length $base;
     $path = '/' . $path if substr($path, 0, 1) ne '/';
@@ -111,8 +114,73 @@ package PlackX::Framework::Router::Engine {
 }
 
 package PlackX::Framework::Router::Engine::Match {
-  sub prefilters  ($self) { }
-  sub postfilters ($self) { }
+  use Plack::Util::Accessor qw(action prefilters postfilters);
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+PlackX::Framework::Router::Engine
+
+
+=head1 DESCRIPTION
+
+This module provides route and global filter matching for PlackX::Framework.
+Please see PlackX::Framework and PlackX::Framework::Router for details of how
+to use routing and filters in your application.
+
+
+=head1 CLASS METHODS
+
+=over 4
+
+=item new
+
+Create a new object.
+
+=item instance
+
+Return a singleton object, creating a new one if one does not exist already.
+
+=back
+
+
+=head1 OBJECT METHODS
+
+=over 4
+
+=item add_route
+
+=item add_global_filter
+
+=item match
+
+=item match_global_filters
+
+=back
+
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item path_with_base($path, $base)
+
+Prefix $path with $base, adding a forward slash if necessary.
+
+=item path_with_method($path, $method|undef)
+
+Prefix $path with a route parameter optionally matching $method.
+
+=item path_with_base_and_method($path, $base, $method|undef)
+
+Combines the above functions.
+
+
+=head1 META
+
+For author, copyright, and license, see PlackX::Framework.
+
