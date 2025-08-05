812c4e3253d15efc9a5da6e03fb80110d9686eb0
Dondi Michael Stroma <dstroma@gmail.com>
2025-08-05 05:07:49 -0400

    Add some pod for RouterEngine role

--- lib/PlackX/Framework/Role/RouterEngine.pm
+++ lib/PlackX/Framework/Role/RouterEngine.pm
@@ -5,3 +5,67 @@ package PlackX::Framework::Role::RouterEngine {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+PlackX::Framework::Role::RouterEngine
+
+
+=head1 SYNOPSIS
+
+    package My::Router::Engine {
+        use Role::Tiny::With;
+        with 'PlackX::Framework::Role::RouterEngine';
+        sub new       { ... }
+        sub instance  { ... }
+        sub match     { ... }
+        sub add_route { ... }
+        sub add_global_filter { ... }
+    }
+
+
+=head1 DESCRIPTION
+
+This module defines a role which is useful in writing a custom Router::Engine
+class for PlackX::Framework.
+
+The Engine class shall be a singleton which returns the same instance for a
+particular class (meaning subclasses should get difference instances) whenever
+new() or instance() is called.
+
+
+=head1 REQUIRED OBJECT METHODS
+
+=over 4
+
+=item new, instance
+
+Return an instance of the class.
+
+=item $obj->match($request)
+
+Using a PlackX::Framework::Request object to find a matching route. The return
+value shall be undef or false if no match is found. If a match is found, a
+hashref should be returned.
+
+The hashref should contain the following keys:
+
+TBD.
+
+=item $obj->add_route(...)
+
+Add a route.
+
+=item $obj->add_global_filter(...)
+
+Add a global filter.
+
+=back
+
+
+=head1 META
+
+For copyright and license, see PlackX::Framework.
+
